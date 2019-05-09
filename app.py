8239b4090c596974f6c96d5f601566f38dac5723
t00189412 <ronan.parkinson@students.ittralee.ie>
2019-05-09 13:41:02 +0100

    Remove web app error

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def add():
     page = '<html><body><h1>'
-    page += generator.addition(4,2)
-    page += '</h1></body></html>'
+    page += '<p> Addition: '
+    page += generator.addition(4,3).__str__()
+    page += '</p></h1></body></html>'
     return page
 
 if __name__ == "__main__":
